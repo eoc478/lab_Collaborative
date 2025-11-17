82a05883e0bf56681fc03d964b7839ed84d5d64d
Claire <eoc478@newschool.edu>
2025-11-17 16:41:08 -0500

    in class commit

---
+++ docs/script.js
@@ -0,0 +1,47 @@
+const socket = io(); //create instance of websocket
+const canvas = document.getElementById("board"); //store the canvas element
+const ctx = canvas.getContext("2d"); //instance of the canvas and set it to 2D dimensions
+
+let drawing = false;
+let lastX, lastY;
+
+//when the user is clicking, they are drawing
+canvas.addEventListener("mousedown", (e) => {
+  drawing = true;
+  [lastX, lastY] = [e.offsetX, e.offsetY]; //take last X point and create a new offset
+});
+
+canvas.addEventListener("mouseup", () => (drawing = false)); 
+
+//if the user is clicking down on the mouse, use the drawLien feature between last poitn adn current point
+canvas.addEventListener("mousemove", (e) => {
+  if (!drawing) return;
+  const [x, y] = [e.offsetX, e.offsetY];
+  drawLine(lastX, lastY, x, y, true);
+  [lastX, lastY] = [x, y];
+});
+
+//canvas methods
+function drawLine(x1, y1, x2, y2, emit) {
+  ctx.beginPath();
+  ctx.moveTo(x1, y1);
+  ctx.lineTo(x2, y2);
+  ctx.stroke();
+
+  //socket emits our draing data as a JSON object, sending out a signal that someone is drawing and send the data on all the lines drawn
+  if (emit) socket.emit("draw", { x1, y1, x2, y2 });
+}
+
+//when socket receives "draw" event, it passes the JSON data to our drawLine function
+socket.on("draw", ({ x1, y1, x2, y2 }) => drawLine(x1, y1, x2, y2, false));
+
+const clearButton = document.getElementById("clear");
+clearButton.addEventListener("click", () => {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  socket.emit("clear");
+});
+
+socket.on("clear", () => {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+});
+
---
+++ server.js
@@ -0,0 +1,36 @@
+import express from 'express';
+import http from 'http';
+import { Server } from 'socket.io';
+
+// Use the port Render provides, or default to 3000 for local dev
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+const server = http.createServer(app);
+
+// Enable CORS so your GH Pages or React app can connect
+const io = new Server(server, {
+  cors: {
+    origin: "*",             // you can restrict this later
+    methods: ["GET", "POST"]
+  }
+});
+
+app.use(express.static("docs")); //tells our server to use the public folder to serve our files (index.html, style.css, script.js);
+
+io.on("connection", (socket) => {
+    console.log("User connected: " + socket.id);
+    
+    //notice a draw input on our client, then broadcast message to everyone else connected that there is a "draw" signal and pass the data
+    socket.on("draw", (data) => {
+        socket.broadcast.emit("draw", data);
+    })
+
+    socket.on("disconnect", () => {
+        console.log("User disconnected: " + socket.id);
+    })
+});
+
+server.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
